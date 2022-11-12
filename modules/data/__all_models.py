50c579290fec469c5ddfe58365128264efaeb0e1
Nikita Aksenov <me@nawinds.top>
2022-11-12 20:11:28 +0300

    Orders tables added

--- modules/data/__all_models.py
+++ modules/data/__all_models.py
@@ -2,3 +2,4 @@
 Importing all DB fields
 """
 from modules.data.variables import Variable
+from modules.data.orders import Order, OrderItem, OrderStats
---
+++ modules/data/orders.py
@@ -0,0 +1,52 @@
+"""
+Order tables in DB
+"""
+import sqlalchemy
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+
+from .db_session import SqlAlchemyBase
+
+
+class Order(SqlAlchemyBase):
+    """
+    Order object linked to 'orders' table of DB
+    """
+    __tablename__ = "orders"
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True, index=True)
+    customer = Column(Integer, default=0)
+    amount = Column(Integer, default=0)
+    total = Column(Integer, default=0)
+    status = Column(Integer, default=0)
+    origin_msg = Column(Integer, unique=True)
+    new_msg = Column(Integer, unique=True)
+    payment_msg = Column(Integer, unique=True)
+    status_msg = Column(Integer, unique=True)
+
+    stats = relationship("OrderStats", back_populates="order")
+    items = relationship("OrderItem", back_populates="order")
+
+
+class OrderItem(SqlAlchemyBase):
+    """
+    Order products object linked to 'order_items' table of DB
+    """
+    __tablename__ = "order_items"
+    id = Column(Integer, primary_key=True, unique=True, autoincrement=True)
+    order_id = Column(sqlalchemy.Integer, ForeignKey("order.id"), index=True)
+    url = Column(String)
+
+    order = relationship("Order", back_populates="items")
+
+
+class OrderStats(SqlAlchemyBase):
+    """
+    Order stats object linked to 'order_stats' table of DB
+    """
+    __tablename__ = "order_stats"
+    order_id = Column(sqlalchemy.Integer, ForeignKey("order.id"), primary_key=True, unique=True, index=True)
+    d_course = Column(Integer)
+    comission = Column(Integer)
+    total = Column(Integer)
+
+    order = relationship("Order", back_populates="stats")
